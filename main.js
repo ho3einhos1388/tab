187e66d0c5895847b29c10a30e8e1cdd2091262c
elmirashokriyan64@gmail.com <elmirashokriyan64@gmail.com>
2025-08-28 11:18:58 +0330

    versiongh

---
+++ main.js
@@ -0,0 +1,6 @@
+const btn1 = document.getElementById("btn1")
+const btn2 = document.getElementById("btn2")
+const btn3 = document.getElementById("btn3")
+const contant1 = document.getElementById("contant1")
+const contant2 = document.getElementById("contant2")
+const contant3 = document.getElementById("contant3")
